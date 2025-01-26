7e1596055fac1d933fcf5e9930d58b7ac4d8f52f
Sam Preston <spreston1022@gmail.com>
2025-01-26 12:13:39 -0700

    Made a simple change to hello world.

--- modules/hello-world.ts
+++ modules/hello-world.ts
@@ -8,7 +8,7 @@ export default async function (request: ZuploRequest, context: ZuploContext) {
   context.log.info(`Hi, from inside your zup!`);
 
   /**
-   * If you want to proxy an API, you can simply something stupid
+   * If you want to proxy an API, you can simply something stupid this way comes
    * return the content of a fetch. Try it by
    * uncommenting the line below.
    */
